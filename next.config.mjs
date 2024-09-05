c416a951d0b830a83ac26a6124c048ebeb959a18
ParraDanielF <parrasalamancadaniel@gmail.com>
2024-09-05 15:43:48 -0500

    stable version

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+      // allowedForwardedHosts: ["localhost:3000"],
+      // ^ You might have to use this property depending on your exact version.
+    }
+  }
+};
 
 export default nextConfig;
